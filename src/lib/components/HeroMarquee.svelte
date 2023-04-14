65c32617eeaf51a0020098a602aced2d8bf0229a
pritishraj04 <pritishraj04@gmail.com>
2023-04-14 16:09:24 +0530

    Marquee added

---
+++ src/lib/components/HeroMarquee.svelte
@@ -0,0 +1,85 @@
+<script>
+	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+	import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+	import '@splidejs/svelte-splide/css';
+	import '@splidejs/splide-extension-auto-scroll/dist/js/splide-extension-auto-scroll.min.js';
+
+	const marqElements = [
+		{
+			src: '/img/svgs/html.svg',
+			alt: 'HTML Logo'
+		},
+		{
+			src: '/img/svgs/css.svg',
+			alt: 'CSS Logo'
+		},
+		{
+			src: '/img/svgs/sveltekit.svg',
+			alt: 'Svelte Logo'
+		},
+		{
+			src: '/img/svgs/reactjs.svg',
+			alt: 'ReactJS Logo'
+		},
+		{
+			src: '/img/svgs/supabase.svg',
+			alt: 'Supabase Logo'
+		},
+		{
+			src: '/img/svgs/mongodb.svg',
+			alt: 'MongoDB Logo'
+		},
+		{
+			src: '/img/svgs/nodejs.svg',
+			alt: 'NodeJS Logo'
+		},
+		{
+			src: '/img/svgs/expressjs.svg',
+			alt: 'ExpressJS Logo'
+		}
+	];
+	const marqueeOption = {
+		pagination: false,
+		arrows: false,
+		gap: '1rem',
+		type: 'loop',
+		drag: 'free',
+		focus: 'center',
+		autoScroll: {
+			speed: 0.5
+		},
+		perPage: 8,
+		breakpoints: {
+			1220: {
+				perPage: 6
+			},
+			930: {
+				perPage: 4
+			},
+			620: {
+				perPage: 2
+			}
+		}
+	};
+</script>
+
+<div class="marquee">
+	<div class="marquee-inner">
+		<Splide extensions={{ AutoScroll }} options={marqueeOption} aria-labelledby="Marquee">
+			{#each marqElements as marq}
+				<SplideSlide>
+					<img class="marqImages" src={marq.src} alt={marq.alt} />
+				</SplideSlide>
+			{/each}
+		</Splide>
+	</div>
+</div>
+
+<style>
+	.marquee {
+		margin-block: 1.36rem;
+	}
+	.marqImages {
+		width: 4rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import HeroMarquee from '$lib/components/HeroMarquee.svelte';
+</script>
+
 <main>
 	<section class="hero">
 		<div class="container">
@@ -10,57 +14,11 @@
 					development.Currently working at Creatiosoft Pvt. Ltd. as a Test Engineer.
 				</p>
 			</div>
-		</div>
-	</section>
-	<section class="hero">
-		<div class="container">
-			<div class="hero-wrapper">
-				<p class="text-accent">Hello, I am</p>
-				<h1 class="text-name">Pritish Raj.</h1>
-				<h2 class="text-work">Software Engineer from New Delhi.</h2>
-				<p class="paragraph">
-					Right now I'm looking for new opportunites in Test Automation and Web
-					development.Currently working at Creatiosoft Pvt. Ltd. as a Test Engineer.
-				</p>
-			</div>
-		</div>
-	</section>
-	<section class="hero">
-		<div class="container">
-			<div class="hero-wrapper">
-				<p class="text-accent">Hello, I am</p>
-				<h1 class="text-name">Pritish Raj.</h1>
-				<h2 class="text-work">Software Engineer from New Delhi.</h2>
-				<p class="paragraph">
-					Right now I'm looking for new opportunites in Test Automation and Web
-					development.Currently working at Creatiosoft Pvt. Ltd. as a Test Engineer.
-				</p>
-			</div>
-		</div>
-	</section>
-	<section class="hero">
-		<div class="container">
-			<div class="hero-wrapper">
-				<p class="text-accent">Hello, I am</p>
-				<h1 class="text-name">Pritish Raj.</h1>
-				<h2 class="text-work">Software Engineer from New Delhi.</h2>
-				<p class="paragraph">
-					Right now I'm looking for new opportunites in Test Automation and Web
-					development.Currently working at Creatiosoft Pvt. Ltd. as a Test Engineer.
-				</p>
-			</div>
-		</div>
-	</section>
-	<section class="hero">
-		<div class="container">
-			<div class="hero-wrapper">
-				<p class="text-accent">Hello, I am</p>
-				<h1 class="text-name">Pritish Raj.</h1>
-				<h2 class="text-work">Software Engineer from New Delhi.</h2>
-				<p class="paragraph">
-					Right now I'm looking for new opportunites in Test Automation and Web
-					development.Currently working at Creatiosoft Pvt. Ltd. as a Test Engineer.
-				</p>
+			<div class="showcase bg-blur-primary bg-blur">
+				<h3 class="secondary-heading">Technologies i am familiar with</h3>
+				<div class="marq">
+					<HeroMarquee />
+				</div>
 			</div>
 		</div>
 	</section>
@@ -84,8 +42,28 @@
 		font-weight: var(--fw-bold);
 		letter-spacing: 4.8px;
 	}
+
 	.text-work {
 		font-size: 2rem;
 		font-weight: var(--fw-semi-bold);
+	}
+	.showcase {
+		margin-top: 8.5625rem;
+		padding: 2.125rem 2.5rem;
+		border-radius: 32px;
+	}
+	.showcase h3 {
+		font-size: 16px;
+	}
+	@media screen and (max-width: 50em) {
+		.text-name {
+			font-size: 2.25rem;
+		}
+		.text-work {
+			font-size: 1.5rem;
+		}
+		.showcase h3 {
+			font-size: 14px;
+		}
 	}
 </style>
