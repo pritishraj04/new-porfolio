1c56db4ab11f3bef419aacdb555ea866ce79a8a9
pritishraj04 <pritishraj04@gmail.com>
2023-04-21 23:10:53 +0530

    cursor update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,42 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '../lib/components/Footer.svelte';
+	import { spring } from 'svelte/motion';
+
+	let innerWidth;
+	let coords = spring(
+		{ x: 0, y: 0 },
+		{
+			stiffness: 0.05,
+			damping: 0.25
+		}
+	);
+	let size = spring(1);
+
+	function handleMousemove(e) {
+		coords.set({ x: e.clientX, y: e.clientY });
+		e.stopPropagation();
+		e.cancelBubble = true;
+	}
+	function handleMouseDown(e) {
+		size.set(2);
+		e.stopPropagation();
+		e.cancelBubble = true;
+	}
+	function handleMouseUp(e) {
+		size.set(1);
+		e.stopPropagation();
+		e.cancelBubble = true;
+	}
 </script>
 
-<Header />
-<slot />
-<Footer />
+<svelte:window bind:innerWidth />
+
+<div on:mousemove={handleMousemove} on:mousedown={handleMouseDown} on:mouseup={handleMouseUp}>
+	<Header />
+	<slot />
+	<Footer />
+	{#if innerWidth > 780}
+		<div class="circle" style={`left: ${$coords.x}px; top: ${$coords.y}px;; scale: ${$size}`} />
+	{/if}
+</div>
