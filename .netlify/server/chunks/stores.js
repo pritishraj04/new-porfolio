7d476844b706e9b563aa36935a27f833f8858326
pritishraj04 <pritishraj04@gmail.com>
2023-05-05 18:13:48 +0530

    Removed all files that are in the .gitignore

--- .netlify/server/chunks/stores.js
+++
@@ -1,9 +0,0 @@
-import { w as writable } from "./index.js";
-let currentTheme = writable("");
-let selectedCategory = writable("all");
-let contactModal = writable(false);
-export {
-  contactModal as a,
-  currentTheme as c,
-  selectedCategory as s
-};
--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.75682b8d.js';
-export const imports = ["_app/immutable/entry/error.svelte.75682b8d.js","_app/immutable/chunks/index.8da94ae9.js","_app/immutable/chunks/stores.102bba40.js","_app/immutable/chunks/singletons.0d62ad34.js","_app/immutable/chunks/index.51f53383.js"];
-export const stylesheets = [];
-export const fonts = [];
