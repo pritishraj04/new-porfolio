60254e4fcb6500505b7f51e349ce682ccd8959f9
pritishraj04 <pritishraj04@gmail.com>
2023-05-23 17:45:30 +0530

    500 fix v1

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,20 +1,20 @@
-import { db } from '$lib/database';
+// import { db } from '$lib/database';
 
 export async function load({ locals, url }) {
-	const works = async () => {
-		return await db.Works.findMany();
-	};
-	const technologies = async () => {
-		return await db.Technologies.findMany();
-	};
-	const categories = async () => {
-		return await db.Categories.findMany();
-	};
+	// const works = async () => {
+	// 	return await db.Works.findMany();
+	// };
+	// const technologies = async () => {
+	// 	return await db.Technologies.findMany();
+	// };
+	// const categories = async () => {
+	// 	return await db.Categories.findMany();
+	// };
 	return {
 		theme: locals.theme,
-		url: url.pathname,
-		works: works(),
-		technologies: technologies(),
-		categories: categories()
+		url: url.pathname
+		// works: works(),
+		// technologies: technologies(),
+		// categories: categories()
 	};
 }
